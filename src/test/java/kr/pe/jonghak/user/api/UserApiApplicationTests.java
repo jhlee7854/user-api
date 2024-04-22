5a998ad8f74ab6d666bf55e997286e0b63f4ab1a
jhlee7854 <jhlee7854@me.com>
2024-04-22 15:05:05 +0900

    Initial commit

---
+++ src/test/java/kr/pe/jonghak/user/api/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.jonghak.user.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
