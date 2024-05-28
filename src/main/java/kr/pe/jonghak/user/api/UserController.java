863f7214b5f5365976d59b8eed5b7159b4f1885c
jhlee7854 <jhlee7854@me.com>
2024-05-28 11:21:17 +0900

    Release v0.0.4
    Modify application using Confluent Kafka

--- src/main/java/kr/pe/jonghak/user/api/UserController.java
+++ src/main/java/kr/pe/jonghak/user/api/UserController.java
@@ -2,20 +2,26 @@ package kr.pe.jonghak.user.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @Slf4j
 public class UserController {
     private final UserRepository userRepository;
+    private static final String TOPIC = "devops-user";
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, KafkaTemplate<String, String> kafkaTemplate) {
         this.userRepository = userRepository;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     @GetMapping("/users")
@@ -29,6 +35,16 @@ public class UserController {
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = userRepository.save(user);
         log.info("saved user: {}", savedUser.getName());
+        String key = "created";
+        String value = savedUser.getEmail();
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(TOPIC, key, value);
+        future.whenComplete((result, ex) -> {
+            if (ex != null) {
+                log.error("error", ex);
+            } else {
+                log.info(String.format("\n\n Produced event to topic %s: key = %-10s value = %s \n\n", TOPIC, key, value));
+            }
+        });
         return ResponseEntity.ok(savedUser);
     }
 }
